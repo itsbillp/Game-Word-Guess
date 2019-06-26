0ae9eabec8fa47c91d5a0130bcd7e760c7aa93cd
Bill Papas <itsbillp@gmail.com>
2019-06-25 20:36:37 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,83 @@
-// Bill's JavaScript for Word Guess Game
+var letters = ["a", "b", "c"];
+
+// This array will hold what we guess
+var guessedLetters = [];
+
+// This variable will be randomly assigned one of the three letters
+var letterToGuess = null;
+
+// This is what we'll use to count down
+var guessesLeft = 9;
+
+// This is the counter for wins/losses
+var wins = 0;
+var losses = 0;
+
+// Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
+  // (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas.
+  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+
+// Function will be called when we reset everything
+var reset = function() {
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+// Execute on page load.
+updateLetterToGuess();
+updateGuessesLeft();
+
+// This function will capture the keyboard clicks.
+document.onkeydown = function(event) {
+  // It's going to reduce the guesses by one
+  guessesLeft--;
+
+  // Lowercase the letter
+  var letter = event.key.toLowerCase();
+
+  // Then add the guess to the guessedLetters array
+  guessedLetters.push(letter);
+
+  // Then its going to run the update functions
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
 
-// Defining the Animals
-    var  animalInput = "Elephant Lion Kangaroo Wombat Dingo Gazelle Zebra Giraffe Crocodile";
+  // We'll check if there's a match.
+  if (letter === letterToGuess) {
 
-// Converting my string into an array
-    var animals = animalInput.split(" "); 
+    // If there is then we win and we'll update the HTML to display the win.
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
 
-console.log(animals)
+    // Then we'll reset the game
+    reset();
+  }
 
-// Finding a Random animal
-    chosenAnimal = Math.floor(Math.random() * (animals.length));
+  // If we are out of guesses...
+  if (guessesLeft === 0) {
 
-console.log(chosenAnimal)
-console.log(animals[chosenAnimal])
-        
-// 1 Writing the "_" to the gamebox div 2 emoving the commas from the chosenWord
-    chosenWord = [];
+    // Then we will loss and we'll update the HTML to display the loss.
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
 
-    for (var i = 0; i < animals[chosenAnimal].length; i++) {
-        chosenWord.push(" _ ");
-    }
-
-console.log(chosenWord)
-
-// 2
-    var chosenWordText = "";
-    for (var i = 0; i < chosenWord.length; i++) {
-        chosenWordText += chosenWord[i];
-    }
-    document.getElementById("gamebox").innerHTML = chosenWordText;
-
-// Record userInput and put it into a variable. Only record letters.
-
-document.onkeydown = function(keyPress) {
-            if(event.keyCode >= 65 && event.keyCode <= 90) {
-                console.log(keyPress.key.toLowerCase());
-            }
-    };
-
-
-
-
- 
+    // Then we'll call the reset.
+    reset();
+  }
+};
